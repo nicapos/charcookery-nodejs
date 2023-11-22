8419a150dedf529e673c1f4d2f558d3ed3154721
nicapos <67004893+nicapos@users.noreply.github.com>
2023-11-22 21:42:32 +0800

    chore: add ESLint and Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import router from "./routes";
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use('/api', router);
+app.use("/api", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript Express!");
