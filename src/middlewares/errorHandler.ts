3eacbd1d2ae7dae4730cbf70386f56f95f31b7c4
nicapos <67004893+nicapos@users.noreply.github.com>
2023-11-24 00:10:41 +0800

    fix: error handler
    note: still can't handle errors in async functions, use a Promise then() for now

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,19 +1,34 @@
-import { Request, Response, NextFunction } from "express";
+import { FirebaseError } from "@firebase/util";
+import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
+import { ZodError } from "zod";
 
-const ErrorHandler = (
-  err: any,
+const ErrorHandler: ErrorRequestHandler = (
+  error: any,
   req: Request,
   res: Response,
   next: NextFunction
-): void => {
-  const status = err.statusCode || 500;
-  const message = err.message || "Something went wrong";
+) => {
+  let statusCode = 500; // Internal Server Error
+  let responsePayload: any = {};
 
-  res.status(status).json({
-    status,
-    message,
-    stack: process.env.NODE_ENV === "development" ? err.stack : {},
-  });
+  if (error instanceof FirebaseError) {
+    statusCode = 400;
+    responsePayload = {
+      name: error.name,
+      code: error.code,
+      message: error.message,
+    };
+  } else if (error instanceof ZodError) {
+    statusCode = 422; // Unprocessable Entity
+    responsePayload = {
+      name: error.name,
+      issues: error.issues,
+    };
+  } else {
+    responsePayload = error;
+  }
+
+  res.status(statusCode).json(responsePayload);
 };
 
 export default ErrorHandler;
