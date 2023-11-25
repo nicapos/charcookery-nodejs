8dcd00eb89157bb1d5811503ebd7bc5bdc31eb46
nicapos <67004893+nicapos@users.noreply.github.com>
2023-11-26 03:24:29 +0800

    refactor: use UserService for controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -9,33 +9,27 @@ import {
 } from "@firebase/auth";
 import { Request, Response } from "express";
 import { LoginCredentialsSchema, TokenCredentialsSchema } from "../docs/types";
-import { auth, db } from "../firebase";
+import { auth } from "../firebase";
 import { handleError } from "../middlewares/errorHandler";
-import { collection, doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
 import {
   AccountType,
   EditableAccountSchema,
   RequestPasswordChangeSchema,
 } from "../schemas/users";
+import UserService from "../services/UserService";
 
 const usersController = {
   getAccountDetails: (req: Request, res: Response) => {
-    const user = getAuth().currentUser;
+    const activeUser = getAuth().currentUser;
 
-    if (user) {
-      const userDocRef = doc(collection(db, "users"), user.uid);
-
-      getDoc(userDocRef)
-        .then((snapshot) => {
-          if (snapshot.exists()) {
-            res.status(200).json(snapshot.data());
-          }
-        })
-        .catch((err) => handleError(res, err));
-    } else {
-      // Middleware should prevent activeUser == null, but just in case:
-      res.status(400).json({ message: "Unauthorized" });
+    if (!activeUser) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
     }
+
+    UserService.getById(activeUser.uid)
+      .then((account: AccountType) => res.status(200).json(account))
+      .catch((err) => handleError(res, err));
   },
 
   createAccount: (req: Request, res: Response) => {
@@ -45,17 +39,7 @@ const usersController = {
       .then((credentials: UserCredential) => {
         const user = credentials.user;
 
-        // Create account in DB
-        const account: AccountType = {
-          name: user.providerData[0].displayName,
-          email: user.email,
-          photo_url: user.providerData[0].photoURL,
-          dietary_restrictions: null,
-        };
-
-        const newDocumentRef = doc(collection(db, "users"), user.uid);
-
-        setDoc(newDocumentRef, account)
+        UserService.create(credentials.user)
           .then(() => res.status(201).json(user))
           .catch((err) => handleError(res, err));
       })
@@ -71,9 +55,7 @@ const usersController = {
 
     if (grant_type === "password") {
       signInWithEmailAndPassword(auth, email, password)
-        .then((credentials) => {
-          res.status(201).json(credentials.user);
-        })
+        .then(({ user }) => res.status(201).json(user))
         .catch((err) => handleError(res, err));
     } else throw Error(`Unknown grant type (passed ${grant_type})`);
   },
@@ -92,76 +74,84 @@ const usersController = {
     const account = EditableAccountSchema.parse(req.body);
     const activeUser = getAuth().currentUser;
 
-    if (activeUser) {
-      const newDocumentRef = doc(collection(db, "users"), activeUser.uid);
-
-      updateDoc(newDocumentRef, account)
-        .then(() => res.status(200).json(account))
-        .catch((err) => handleError(res, err));
-    } else {
-      // Middleware should prevent activeUser == null, but just in case:
+    if (!activeUser) {
       res.status(401).json({ message: "Unauthorized" });
+      return;
     }
+
+    UserService.updateById(activeUser.uid, account)
+      .then(() => res.status(200).json(account))
+      .catch((err) => handleError(res, err));
   },
 
   sendVerificationEmail: (req: Request, res: Response) => {
     const activeUser = getAuth().currentUser;
 
-    if (activeUser) {
-      if (!activeUser.emailVerified) {
-        sendEmailVerification(activeUser)
-          .then(() =>
-            res.status(200).json({ message: "Verification email sent" })
-          )
-          .catch((err) => handleError(res, err));
-      } else {
-        res.status(400).json({ message: "Account is already verified" });
-      }
-    } else {
-      // Middleware should prevent activeUser == null, but just in case:
+    if (!activeUser) {
       res.status(401).json({ message: "Unauthorized" });
+      return;
     }
+
+    if (activeUser.emailVerified) {
+      res.status(400).json({ message: "Account is already verified" });
+      return;
+    }
+
+    sendEmailVerification(activeUser)
+      .then(() => res.status(200).json({ message: "Verification email sent" }))
+      .catch((err) => handleError(res, err));
   },
 
   requestPasswordChange: (req: Request, res: Response) => {
     const { email } = RequestPasswordChangeSchema.parse(req.body);
     const user = getAuth().currentUser;
 
-    if (user && email != getUserEmail(user)) {
+    if (!user) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+
+    if (email != getUserEmail(user)) {
       res.status(403).json({
         message: `Unauthorized. Provided email doesn't match account email (passed ${email}).`,
       });
       return;
     }
 
-    // Check if user is verified
-    if (user && user.emailVerified) {
-      sendPasswordResetEmail(auth, email)
-        .then(() => {
-          res.status(200).json({
-            message: "Password reset link sent to your email.",
-          });
-        })
-        .catch((err) => handleError(res, err));
-    } else {
+    if (!user.emailVerified) {
       res.status(403).json({
         message: "Unauthorized. Verify your email first.",
       });
+      return;
     }
+
+    sendPasswordResetEmail(auth, email)
+      .then(() =>
+        res.status(200).json({
+          message: "Password reset link sent to your email.",
+        })
+      )
+      .catch((err) => handleError(res, err));
   },
 
   deleteAccount: (req: Request, res: Response) => {
     const activeUser = getAuth().currentUser;
 
-    if (activeUser) {
-      activeUser
-        .delete()
-        .then(() => res.status(204).end())
-        .catch((err) => handleError(res, err));
-    } else {
-      // Middleware should prevent activeUser == null, but just in case:
+    if (!activeUser) {
       res.status(401).json({ message: "Unauthorized" });
+      return;
     }
+
+    const userId = activeUser.uid;
+
+    UserService.deleteById(userId)
+      .then(() =>
+        activeUser
+          .delete()
+          .then(() => res.status(204).end())
+          .catch((err) => handleError(res, err))
+      )
+      .catch((err) => handleError(res, err));
   },
 };
 
